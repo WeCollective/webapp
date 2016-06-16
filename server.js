93f1f1f29883fe6946f9b5eb2d4e3b43a02933e3
MikeC1995 <mike_christensen@btinternet.com>
2016-06-16 18:51:45 +0100

    added helmet protection to secure against common web vulnerabilities by setting headers

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 // REQUIRE MODULES
 var express    = require("express");          // call express
 var app        = express();                   // define our app using express
+var helmet = require('helmet');               // protect against common web vulnerabilities
 
 // SET ENVIRONMENT AND PORT
 var env = (process.env.NODE_ENV || "development");
 var port = process.env.PORT || 8081;
+
+// MIDDLEWARE
+app.use(helmet());
 
 // SERVE THE NODE MODULES FOLDER
 app.use("/dependencies/node", express.static(__dirname + "/node_modules"));
