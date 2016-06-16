f49cc239910bad5909cb44497f073bf77339652d
MikeC1995 <mike_christensen@btinternet.com>
2016-06-16 12:43:48 +0100

    output messages when deploying

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,7 @@ module.exports = function(grunt) {
           } else {
             return '';
           }
-          return 'git checkout ' + checkout + ' && eb deploy';
+          return 'echo Checking out ' + checkout + ' && git checkout ' + checkout + ' && echo Deploying... && eb deploy';
         }
       }
     }
