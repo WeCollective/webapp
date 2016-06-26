d699e28f5d197bf41fdccc6156c9a56bfac36fc6
MikeC1995 <mike_christensen@btinternet.com>
2016-06-26 19:41:36 +0100

    slide up and fade in modal animation

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module('wecoApp', ['config', 'ui.router', 'api']);
+var app = angular.module('wecoApp', ['config', 'ui.router', 'ngAnimate', 'api']);
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
