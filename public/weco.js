46cf59597e71d98abfd0bf39c2d9884a2416c074
MikeC1995 <mike_christensen@btinternet.com>
2016-06-26 12:47:19 +0100

    added new homepage image, login/signup button disabled once clicked until response recieved

--- public/weco.js
+++ public/weco.js
@@ -259,6 +259,7 @@ var app = angular.module('wecoApp');
 app.controller('authController', ['$scope', '$state', 'User', function($scope, $state, User) {
   $scope.credentials = {};
   $scope.user = User.me;
+  $scope.isLoading = false;
 
   $scope.isLoginForm = function() {
     return $state.current.name == 'auth.login';
@@ -267,24 +268,29 @@ app.controller('authController', ['$scope', '$state', 'User', function($scope, $
   function login() {
     User.login($scope.credentials).then(function() {
       // successful login; redirect to home page
+      $scope.isLoading = false;
       $state.go('weco.home');
     }, function() {
       // TODO: pretty error
       alert('Unable to log in!');
+      $scope.isLoading = false;
     });
   }
 
   function signup() {
     User.signup($scope.credentials).then(function() {
       // successful signup; redirect to home page
+      $scope.isLoading = false;
       $state.go('weco.home');
     }, function() {
       // TODO: pretty error
       alert('Unable to sign up!');
+      $scope.isLoading = false;
     });
   }
 
   $scope.submit = function() {
+    $scope.isLoading = true;
     if($scope.isLoginForm()) {
       login();
     } else {
