f77e9ef1568a548ef19fb4c90a9f568b6599aa97
MikeC1995 <mike_christensen@btinternet.com>
2016-07-04 12:02:20 +0100

    updated home button in nav bar

--- public/weco.js
+++ public/weco.js
@@ -344,7 +344,7 @@ app.directive('tabs', ['$state', function($state) {
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://weco-api-prod.eu-west-1.elasticbeanstalk.com/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/'})
 
 ;
 var api = angular.module('api', ['ngResource']);
