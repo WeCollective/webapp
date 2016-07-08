95e4d880397e624aed88a7a0b287f6aa9c3dbd6e
MikeC1995 <mike_christensen@btinternet.com>
2016-07-08 15:56:41 +0100

    automatic build commit

--- public/weco.js
+++ public/weco.js
@@ -535,7 +535,7 @@ app.directive('tabs', ['$state', function($state) {
 
  angular.module('config', [])
 
-.constant('ENV', {name:'local',apiEndpoint:'http://localhost:8080/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/'})
 
 ;
 var api = angular.module('api', ['ngResource']);
