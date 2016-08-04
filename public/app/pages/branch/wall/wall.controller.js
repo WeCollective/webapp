4ff6c8627e603b344a77600edcd1f335546b4477
Michael Christensen <Michael Christensen>
2016-08-04 12:36:16 +0100

    post template implemented for listing on branch wall

--- public/app/pages/branch/wall/wall.controller.js
+++ public/app/pages/branch/wall/wall.controller.js
@@ -2,12 +2,12 @@
 
 var app = angular.module('wecoApp');
 app.controller('wallController', ['$scope', '$state', '$timeout', 'Branch', function($scope, $state, $timeout, Branch) {
-  $scope.isLoading = true;
-  $scope.posts = [];
+  $scope.isLoading = false;
+  $scope.posts = [1, 2];
 
   // watch for change in drop down menu time filter selection
   $scope.selectedTimeItemIdx = 0;
   $scope.$watch('selectedTimeItemIdx', function () {
-    
+
   });
 }]);
