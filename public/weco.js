d60689a2b12beadbc15f147e1c1da57805b941fa
Michael Christensen <Michael Christensen>
2016-08-16 15:37:36 +0100

    time since comment posted is now displayed

--- public/weco.js
+++ public/weco.js
@@ -147,6 +147,35 @@ app.directive('commentThread', ['Comment', '$timeout', function(Comment, $timeou
     },
     templateUrl: '/app/components/comment-thread/comment-thread.view.html',
     link: function ($scope) {
+      // compute a string indicate time since post
+      $scope.timeSince = function(date) {
+        var msPerMinute = 60 * 1000;
+        var msPerHour = msPerMinute * 60;
+        var msPerDay = msPerHour * 24;
+        var msPerMonth = msPerDay * 30;
+        var msPerYear = msPerDay * 365;
+
+        var elapsed = new Date().getTime() - new Date(date);
+
+        if (elapsed < msPerMinute) {
+          return Math.round(elapsed/1000) + ' seconds ago';
+        }
+        if (elapsed < msPerHour) {
+          return Math.round(elapsed/msPerMinute) + ' minutes ago';
+        }
+        if (elapsed < msPerDay ) {
+          return Math.round(elapsed/msPerHour ) + ' hours ago';
+        }
+        if (elapsed < msPerMonth) {
+          return Math.round(elapsed/msPerDay) + ' days ago';
+        }
+        if (elapsed < msPerYear) {
+          return Math.round(elapsed/msPerMonth) + ' months ago';
+        }
+        return Math.round(elapsed/msPerYear ) + ' years ago';
+      };
+
+
       // Asynchronously load the comments's data one by one
       // The 'scope' is the bound comment object onto which the data should be attached
       function loadCommentData(scope, comments, idx) {
