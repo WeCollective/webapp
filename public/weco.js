ea52ef911f3ad429208941dbcf6df3e67badeeea
Michael Christensen <Michael Christensen>
2016-09-19 13:17:05 +0100

    automatic build commit

--- public/weco.js
+++ public/weco.js
@@ -1948,7 +1948,7 @@ app.directive('writeComment', function() {
 
  angular.module('config', [])
 
-.constant('ENV', {name:'local',apiEndpoint:'http://localhost:8080/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/'})
 
 ;
 var api = angular.module('api', ['ngResource']);
