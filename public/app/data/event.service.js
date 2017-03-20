7162c42a4f49f5908ff02d5228fbf62ff6bba429
Michael Christensen <Michael_Christensen@discovery.com>
2017-03-20 14:46:23 +0000

    create event service to abstract out use of $rootScope event broadcast with a defined set of events and wrapping callbacks in timeout to updaet the view

---
+++ public/app/data/event.service.js
@@ -0,0 +1,31 @@
+import Injectable from 'utils/injectable';
+
+class EventService extends Injectable {
+  constructor(...injections) {
+    super(EventService.$inject, injections);
+
+    this.events = {
+      CHANGE_USER: 'CHANGE_USER'
+    };
+  }
+
+  emit(event) {
+    let eventExists = false;
+    for(let idx in this.events) {
+      if(this.events[idx] === event) {
+        eventExists = true;
+        break;
+      }
+    }
+    if(eventExists) {
+      this.$rootScope.$broadcast(event);
+    }
+  }
+
+  on(event, callback) {
+    this.$rootScope.$on(event, () => { return this.$timeout(callback); });
+  }
+}
+EventService.$inject = ['$rootScope', '$timeout'];
+
+export default EventService;
