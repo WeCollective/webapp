3d9dc208a652c1da4656d6ebfb156971ae243942
Mike Christensen <mike_christensen@btinternet.com>
2017-03-27 21:14:10 +0100

    move mod log templates to separate files

--- public/app/components/mod-log-entry/mod-log-entry.directive.js
+++ public/app/components/mod-log-entry/mod-log-entry.directive.js
@@ -1,20 +1,3 @@
-/* Directive for dynamically generating a template for the display of an entry
-** to the mod log.
-** Mod log entry data from the server is passed in as 'entry' and rendered HTML
-** describing the visualisation of this entry is generated.
-**
-** The following strings are produced for each entry type:
-**
-** addmod/remove mod
-** <mod1> added/removed <mod2> as a moderator.
-**
-** make-subbranch-request
-** <childmod> made a subbranch request to <parentbranch>.
-**
-** answer-subbranch-request
-** <parentmod> accepted/rejected a subbranch request to <parentbranch> made by <childmod> on <childbranch>.
-*/
-
 import Injectable from 'utils/injectable';
 
 class ModLogEntryComponent extends Injectable {
@@ -28,58 +11,16 @@ class ModLogEntryComponent extends Injectable {
     };
   }
 
-  getLogActionVerb(action) {
-    if(action === 'addmod') {
-      return 'added';
-    } else if(action === 'removemod') {
-      return 'removed';
-    } else {
-      return '';
-    }
-  }
-
-  getTemplate(entry) {
-    var templateStr = '<div class="time">{{ entry.date | date: \'dd MMMM yyyy HH:mm\' }}</div>';
-
-    switch (entry.action) {
-      case 'removemod':
-      case 'addmod':
-        return templateStr +
-          '<div class="entry">' +
-            '<a ui-sref="weco.profile.about({ username: entry.username })">{{ entry.username }}</a>' +
-            ' ' + this.getLogActionVerb(entry.action) + ' ' +
-            '<a ui-sref="weco.profile.about({ username: entry.data })">{{ entry.data }}</a>' +
-            ' as a moderator.' +
-          '</div>';
-      case 'make-subbranch-request':
-        return templateStr +
-          '<div class="entry">' +
-            '<a ui-sref="weco.profile.about({ username: entry.username })">{{ entry.username }}</a>' +
-            ' made a Child Branch Request to ' +
-            '<a ui-sref="weco.branch.nucleus.about({ branchid: entry.data })">{{ entry.data }}</a>.' +
-          '</div>';
-      case 'answer-subbranch-request':
-        var data = JSON.parse(entry.data);
-        return templateStr +
-          '<div class="entry">' +
-            '<a ui-sref="weco.profile.about({ username: entry.username })">{{ entry.username }}</a> ' +
-             data.response + 'ed a Child Branch Request to ' +
-            '<a ui-sref="weco.branch.nucleus.about({ branchid: \'' + data.parentid + '\' })">' + data.parentid + '</a>' +
-            ' made by ' +
-            '<a ui-sref="weco.profile.about({ username: \'' + data.childmod + '\' })">' + data.childmod + '</a>' +
-            ' from ' +
-            '<a ui-sref="weco.branch.nucleus.about({ branchid: \'' + data.childid + '\' })">' + data.childid + '</a>.' +
-          '</div>';
-      default:
-        return '';
-    }
-  }
-
   link(scope, element, attrs) {
-    element.html(this.getTemplate(scope.entry));
-    this.$compile(element.contents())(scope);
+    if(scope.entry.action === 'answer-subbranch-request') scope.entry.data = JSON.parse(scope.entry.data);
+    this.$templateRequest(`/app/components/mod-log-entry/${ scope.entry.action }.template.html`).then((template) => {
+      element.html(template);
+      this.$compile(element.contents())(scope);
+    }, () => {
+      console.error('Unable to get mod-log-entry template.');
+    });
   }
 }
-ModLogEntryComponent.$inject = ['$compile'];
+ModLogEntryComponent.$inject = ['$compile', '$templateRequest'];
 
 export default ModLogEntryComponent;
