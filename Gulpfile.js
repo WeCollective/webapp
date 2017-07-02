b5eacf81354ae08a45c4f55e2b2daede9ac5bc69
Lubos <lmenus@lmen.us>
2017-07-02 21:31:18 +0100

    Build commit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,15 @@
-const gulp  = require('gulp');
+const gulp = require('gulp');
 const yargs = require('yargs');
 const clean = require('gulp-clean');
-const del   = require('del');
-const fs    = require('fs');
-const gutil   = require('gulp-util');
-const jshint  = require('gulp-jshint');
-const less    = require('gulp-less');
-const logger  = require('./console-logger');
+const del = require('del');
+const fs = require('fs');
+const gutil = require('gulp-util');
+const jshint = require('gulp-jshint');
+const less = require('gulp-less');
+const logger = require('./console-logger');
 const nodemon = require("gulp-nodemon");
-const path    = require('path');
-const rename  = require('gulp-rename');
+const path = require('path');
+const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const runSequence = require('run-sequence');
 const webpack = require('webpack');
@@ -18,9 +18,9 @@ const DEFAULT_ENV = 'development';
 let environment = process.env.NODE_ENV || DEFAULT_ENV;
 
 const PUBLIC_DIR = path.join(__dirname, 'public');
-const APP_DIR    = path.join(PUBLIC_DIR, 'app');
+const APP_DIR = path.join(PUBLIC_DIR, 'app');
 const ASSETS_DIR = path.join(PUBLIC_DIR, 'assets');
-const DEST_DIR   = path.join(PUBLIC_DIR, 'dist');
+const DEST_DIR = path.join(PUBLIC_DIR, 'dist');
 
 const GULP_ENV_CONFIG_FILE_DIR = './'
 const GULP_ENV_CONFIG_FILE_PATH = `${GULP_ENV_CONFIG_FILE_DIR}.gulp-env`;
@@ -37,7 +37,7 @@ function fileFromString (opts = {}) {
       base: '',
       contents: new Buffer(opts.body),
       cwd: '',
-      path: opts.name
+      path: opts.name,
     }));
     this.push(null);
   }
@@ -112,14 +112,17 @@ gulp.task('configEnvironment', () => {
   fs.readFile(GULP_ENV_CONFIG_FILE_PATH, 'utf-8', (err, file_body) => {
     const cliEnv = err || _firstRun ? (yargs.argv.env || environment) : file_body;
 
-    if ('production' === cliEnv) {
+    if (cliEnv === 'production') {
       environment = 'production';
     }
-    else if ('dev' === cliEnv) {
+    else if (cliEnv === 'dev') {
       environment = 'development';
     }
-    else if ('local' === cliEnv) {
+    else if (cliEnv === 'local') {
       environment = 'local';
+    }
+    else if (cliEnv === 'test') {
+      environment = 'test';
     }
     
     if (cliEnv) {
@@ -174,14 +177,17 @@ gulp.task('nodemon', () => {
 gulp.task('replaceTemplateStrings:config', () => {
   let apiEndpoint;
   
-  if ('local' === environment) {
+  if (environment === 'local') {
     apiEndpoint = 'http://localhost:8080/v1';
   }
-  else if ('development' === environment) {
+  else if (environment === 'development') {
     apiEndpoint = 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1';
   }
-  else if ('production' === environment) {
+  else if (environment === 'production') {
     apiEndpoint = 'https://wecoapi.com/v1';
+  }
+  else if (environment === 'test') {
+    apiEndpoint = 'https://api-prod-test.eu-west-1.elasticbeanstalk.com/v1';
   }
 
   logger.log(`Using ${apiEndpoint} as the endpoint...`, '1F4CD');
@@ -197,14 +203,17 @@ gulp.task('replaceTemplateStrings:index', () => {
   const wecoAppScript = 'production' === environment ? 'bundle.min.js' : 'bundle.js';
   let socketIOEndpoint;
 
-  if ('local' === environment) {
+  if (environment === 'local') {
     socketIOEndpoint = 'http://localhost:8080/socket.io/socket.io.js';
   }
-  else if ('development' === environment) {
+  else if (environment === 'development') {
     socketIOEndpoint = 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/socket.io/socket.io.js';
   }
-  else if ('production' === environment) {
+  else if (environment === 'production') {
     socketIOEndpoint = 'https://wecoapi.com/socket.io/socket.io.js';
+  }
+  else if (environment === 'test') {
+    socketIOEndpoint = 'https://api-prod-test.eu-west-1.elasticbeanstalk.com/socket.io/socket.io.js';
   }
 
   return gulp.src([path.join(PUBLIC_DIR, 'index.template.html')])
@@ -215,7 +224,7 @@ gulp.task('replaceTemplateStrings:index', () => {
 });
 
 gulp.task('webpack', done => {
-  WEBPACK_CONFIG.output.filename = 'production' === environment ? 'bundle.min.js' : 'bundle.js';
+  WEBPACK_CONFIG.output.filename = environment === 'production' ? 'bundle.min.js' : 'bundle.js';
 
   webpack(WEBPACK_CONFIG, (err, stats) => {
       if (err) throw new gutil.PluginError('webpack', err);
