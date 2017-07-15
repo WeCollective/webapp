3098c6af50f43073ff2f3cbb6f55bad79b561a80
Lubos <lmenus@lmen.us>
2017-07-15 22:49:33 +0100

    Build commit

--- public/app/env.config.js
+++ public/app/env.config.js
@@ -1,7 +1,7 @@
 /* Template file from which env.config.js is generated */
 let ENV = {
-  apiEndpoint: 'http://localhost:8080/v1',
-  name: 'local',
+  apiEndpoint: 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1',
+  name: 'development',
 };
 
 export default ENV;
