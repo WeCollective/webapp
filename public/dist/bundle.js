bd00dfffea1e6d2fda763ebdc29e5274184e8b70
Lubos <lmenus@lmen.us>
2017-07-20 14:06:18 +0100

    Build commit

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -4629,9 +4629,9 @@ const constants = {
 "use strict";
 /* Template file from which env.config.js is generated */
 const ENV = {
-  apiEndpoint: 'http://localhost:8080/v1',
+  apiEndpoint: 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1',
   debugAnalytics: true,
-  name: 'local'
+  name: 'development'
 };
 
 /* harmony default export */ __webpack_exports__["a"] = (ENV);
@@ -65582,16 +65582,16 @@ class AnswerEditorController extends __WEBPACK_IMPORTED_MODULE_0_utils_injectabl
   constructor(...injections) {
     super(AnswerEditorController.$inject, injections);
 
-    this.newAnswer = '';
+    this.answer = '';
   }
 
   addAnswer() {
-    if (this.answers.includes(this.newAnswer) || this.newAnswer === '') {
+    if (this.answers.includes(this.answer) || this.answer === '') {
       return;
     }
 
-    this.answers.push(this.newAnswer);
-    this.newAnswer = '';
+    this.answers.push(this.answer);
+    this.answer = '';
   }
 
   removeAnswer(answer) {
@@ -65606,60 +65606,6 @@ class AnswerEditorController extends __WEBPACK_IMPORTED_MODULE_0_utils_injectabl
 AnswerEditorController.$inject = [];
 
 /* harmony default export */ __webpack_exports__["a"] = (AnswerEditorController);
-
-/*
-import Injectable from 'utils/injectable';
-
-class TestController extends Injectable {
-  constructor(...injections) {
-    super(TestController.$inject, injections);
-
-    this.errorMessage = '';
-    this.tag = '';
-  }
-
-  addTag() {
-    this.errorMessage = '';
-
-    if (this.items.length >= this.max() || !this.tag) return;
-
-    // Do not allow spaces in the tags.
-    this.tag = this.tag.split(' ').join('').toLowerCase();
-
-    if (this.items.includes(this.tag)) return;
-
-    this.items.push(this.tag);
-    this.tag = '';
-  }
-
-  handleInputChange() {
-    const length = this.tag.length;
-
-    // Do not allow spaces in the tags.
-    this.tag = this.tag.split(' ').join('');
-
-    if (this.tag[length - 1] === ',') {
-      this.tag = this.tag.substring(0, length - 1);
-      this.addTag();
-    }
-  }
-
-  removeTag(tag) {
-    const tagIndex = this.items.indexOf(tag);
-
-    if (tagIndex !== -1) {
-      this.items.splice(tagIndex, 1);
-    }
-
-    this.errorMessage = '';
-  }
-}
-
-TestController.$inject = [];
-
-export default TestController;
-
-*/
 
 /***/ }),
 /* 189 */
