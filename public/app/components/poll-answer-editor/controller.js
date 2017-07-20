cb7dcde09ec7c8f571fa65f23c120f71bbfc452a
Lubos <lmenus@lmen.us>
2017-07-20 14:05:59 +0100

    Clean up dead code

--- public/app/components/poll-answer-editor/controller.js
+++ public/app/components/poll-answer-editor/controller.js
@@ -4,16 +4,16 @@ class AnswerEditorController extends Injectable {
   constructor(...injections) {
     super(AnswerEditorController.$inject, injections);
 
-    this.newAnswer = '';
+    this.answer = '';
   }
 
   addAnswer() {
-    if (this.answers.includes(this.newAnswer) || this.newAnswer === '') {
+    if (this.answers.includes(this.answer) || this.answer === '') {
       return;
     }
 
-    this.answers.push(this.newAnswer);
-    this.newAnswer = '';
+    this.answers.push(this.answer);
+    this.answer = '';
   }
 
   removeAnswer(answer) {
@@ -28,57 +28,3 @@ class AnswerEditorController extends Injectable {
 AnswerEditorController.$inject = [];
 
 export default AnswerEditorController;
-
-/*
-import Injectable from 'utils/injectable';
-
-class TestController extends Injectable {
-  constructor(...injections) {
-    super(TestController.$inject, injections);
-
-    this.errorMessage = '';
-    this.tag = '';
-  }
-
-  addTag() {
-    this.errorMessage = '';
-
-    if (this.items.length >= this.max() || !this.tag) return;
-
-    // Do not allow spaces in the tags.
-    this.tag = this.tag.split(' ').join('').toLowerCase();
-
-    if (this.items.includes(this.tag)) return;
-
-    this.items.push(this.tag);
-    this.tag = '';
-  }
-
-  handleInputChange() {
-    const length = this.tag.length;
-
-    // Do not allow spaces in the tags.
-    this.tag = this.tag.split(' ').join('');
-
-    if (this.tag[length - 1] === ',') {
-      this.tag = this.tag.substring(0, length - 1);
-      this.addTag();
-    }
-  }
-
-  removeTag(tag) {
-    const tagIndex = this.items.indexOf(tag);
-
-    if (tagIndex !== -1) {
-      this.items.splice(tagIndex, 1);
-    }
-
-    this.errorMessage = '';
-  }
-}
-
-TestController.$inject = [];
-
-export default TestController;
-
-*/
