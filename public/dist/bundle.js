beb76fb02921693ea8acebc1f3a7fba3e7593a65
Lubos <lmenus@lmen.us>
2017-09-01 12:39:31 +0100

    Fix wall loading

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -67744,12 +67744,17 @@ class BranchWallController extends __WEBPACK_IMPORTED_MODULE_0_utils_injectable_
       }
     };
     this.isLoading = false;
-    // To stop sending requests once we hit the bottom of posts.
-    this.lastFetchedPostId = false;
+    this.lastRequest = {
+      id: undefined,
+      lastId: undefined,
+      postType: undefined,
+      sortBy: undefined,
+      statType: undefined,
+      timeAfter: undefined
+    };
     this.posts = [];
 
     this.callbackDropdown = this.callbackDropdown.bind(this);
-    this.callbackLoad = this.callbackLoad.bind(this);
     this.callbackScroll = this.callbackScroll.bind(this);
     this.getPosts = this.getPosts.bind(this);
     this.getPostType = this.getPostType.bind(this);
@@ -67757,62 +67762,72 @@ class BranchWallController extends __WEBPACK_IMPORTED_MODULE_0_utils_injectable_
     this.getStatType = this.getStatType.bind(this);
     this.getTimeAfter = this.getTimeAfter.bind(this);
 
+    // Do the initial load so the loader appears straight away.
+    this.getPosts();
+
     const listeners = [];
     listeners.push(this.$rootScope.$watch(() => this.controls.postType.selectedIndex, this.callbackDropdown));
     listeners.push(this.$rootScope.$watch(() => this.controls.sortBy.selectedIndex, this.callbackDropdown));
     listeners.push(this.$rootScope.$watch(() => this.controls.statType.selectedIndex, this.callbackDropdown));
     listeners.push(this.$rootScope.$watch(() => this.controls.timeRange.selectedIndex, this.callbackDropdown));
-    listeners.push(this.EventService.on(this.EventService.events.CHANGE_BRANCH_PREFETCH, this.callbackLoad));
+    listeners.push(this.EventService.on(this.EventService.events.CHANGE_BRANCH_PREFETCH, () => this.getPosts()));
     listeners.push(this.EventService.on(this.EventService.events.SCROLLED_TO_BOTTOM, this.callbackScroll));
     this.$scope.$on('$destroy', () => listeners.forEach(deregisterListener => deregisterListener()));
   }
 
   callbackDropdown(newValue, oldValue) {
-    if (newValue !== oldValue) this.callbackLoad();
-  }
-
-  callbackLoad() {
-    if (!this.$state.current.name.includes('weco.branch.wall')) return;
-    this.getPosts();
+    if (newValue !== oldValue) this.getPosts();
   }
 
   callbackScroll(name) {
-    if (name === 'WallScrollToBottom' && this.posts.length > 0) {
-      this.getPosts(this.posts[this.posts.length - 1].id);
+    const items = this.posts.length;
+    if (name === 'WallScrollToBottom' && items > 0) {
+      this.getPosts(this.posts[items - 1].id);
     }
   }
 
-  // return the correct ui-sref string for when the specified post is clicked
+  // Return the correct ui-sref string.
   getLink(post) {
-    if (post.type === 'text' || post.type === 'poll') {
-      return this.$state.href('weco.branch.post', { postid: post.id });
+    const correctUiSrefForTypes = ['poll', 'text'];
+    const postid = post.id;
+
+    if (correctUiSrefForTypes.includes(post.type)) {
+      return this.$state.href('weco.branch.post', { postid });
     }
 
     return post.text;
   }
 
   getPosts(lastId) {
-    let posts = this.posts;
-
-    if (this.isLoading === true || lastId === this.lastFetchedPostId) {
+    if (this.isLoading === true || !this.$state.current.name.includes('weco.branch.wall')) {
       return;
     }
 
-    this.isLoading = true;
-
-    if (lastId) {
-      this.lastFetchedPostId = lastId;
-    }
-
-    const id = this.BranchService.branch.id;
+    const id = this.$state.params.branchid;
+    const lr = this.lastRequest;
     const postType = this.getPostType();
     const sortBy = this.getSortBy();
     const statType = this.getStatType();
     const timeAfter = this.getTimeAfter();
 
+    // Don't send the request if nothing changed.
+    if (lr.id === id && lr.lastId === lastId && lr.postType === postType && lr.sortBy === sortBy && lr.statType === statType && lr.timeAfter === timeAfter) {
+      return;
+    }
+
+    // Update the last request values to compare with the next call.
+    this.lastRequest.id = id;
+    this.lastRequest.lastId = lastId;
+    this.lastRequest.postType = postType;
+    this.lastRequest.sortBy = sortBy;
+    this.lastRequest.statType = statType;
+    this.lastRequest.timeAfter = timeAfter;
+
+    this.isLoading = true;
+
     this.BranchService.getPosts(id, timeAfter, sortBy, statType, postType, lastId, false).then(newPosts => this.$timeout(() => {
       // If lastId was specified, we are fetching more posts, so append them.
-      posts = lastId ? posts.concat(newPosts) : newPosts;
+      const posts = lastId ? this.posts.concat(newPosts) : newPosts;
       this.posts = posts;
 
       // 30 is the length of the posts response sent back by server.
