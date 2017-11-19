c8f77c00febdd17168d39f4b5a2dd7973f803171
Lubos <lmenus@lmen.us>
2017-11-19 13:49:03 +0000

    Build commit

--- public/app/env.config.js
+++ public/app/env.config.js
@@ -1,6 +1,6 @@
 /* Template file from which env.config.js is generated */
 const ENV = {
-  apiEndpoint: 'http://localhost:8080/v1',
+  apiEndpoint: 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1',
   debugAnalytics: false,
   name: 'development',
 };
