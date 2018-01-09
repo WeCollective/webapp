6ceec70e353c4f9d3c9934c174ff7d03c60c0c12
lmenus <lmenus@lmen.us>
2018-01-09 20:14:19 +0000

    Build commit

--- public/app/env.config.js
+++ public/app/env.config.js
@@ -1,8 +1,8 @@
 /* Template file from which env.config.js is generated */
 const ENV = {
-  apiEndpoint: 'http://localhost:8080/v1',
+  apiEndpoint: 'https://wecoapi.com/v1',
   debugAnalytics: false,
-  name: 'development',
+  name: 'production',
 };
 
 export default ENV;
