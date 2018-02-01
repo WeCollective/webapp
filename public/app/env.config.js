d665d075df14c5a70a94b5f16d738baa6ac6ddef
lmenus <lmenus@lmen.us>
2018-02-01 23:35:01 +0000

    Fixed brief blank template

--- public/app/env.config.js
+++ public/app/env.config.js
@@ -1,6 +1,6 @@
 /* Template file from which env.config.js is generated */
 const ENV = {
-  apiEndpoint: 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1',
+  apiEndpoint: 'https://localhost:8080/v1',
   debugAnalytics: false,
   name: 'development',
 };
