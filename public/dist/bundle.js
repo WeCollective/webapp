842a37a354117d8af5ce29f37da20db4e808ed39
lmenus <lmenus@lmen.us>
2018-02-02 22:41:29 +0000

    Build commit

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -7357,7 +7357,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 /* Template file from which env.config.js is generated */
 var ENV = {
-  apiEndpoint: 'https://localhost:8080/v1',
+  apiEndpoint: 'http://api-dev.eu9ntpt33z.eu-west-1.elasticbeanstalk.com/v1',
   debugAnalytics: false,
   name: 'development'
 };
